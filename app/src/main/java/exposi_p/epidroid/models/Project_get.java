1cdfb008d51663416c9881dc1d11fd212777a42d
StuartSulaski <stuartsulaski@gmail.com>
2016-01-30 22:53:34 +0100

    j'ai fait le gros du travail

---
+++ app/src/main/java/exposi_p/epidroid/models/Project_get.java
@@ -0,0 +1,399 @@
+package exposi_p.epidroid.models;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Platy on 30/01/2016.
+ */
+public class Project_get {
+    @SerializedName("scolaryear")
+    private String scolaryear;
+    @SerializedName("codemodule")
+    private String codemodule;
+    @SerializedName("codeinstance")
+    private String codeinstance;
+    @SerializedName("codeacti")
+    private String codeacti;
+    @SerializedName("instance_location")
+    private String instance_location;
+    @SerializedName("module_title")
+    private String module_title;
+    @SerializedName("id_activitie")
+    private String id_activitie;
+    @SerializedName("project_title")
+    private String project_title;
+    @SerializedName("type_title")
+    private String type_title;
+    @SerializedName("type_code")
+    private String type_code;
+    @SerializedName("register")
+    private String register;
+    @SerializedName("register_by_bloc")
+    private String register_by_bloc;
+    @SerializedName("register_prof")
+    private String register_prof;
+    @SerializedName("nb_min")
+    private Integer nb_min;
+    @SerializedName("nb_max")
+    private Integer nb_max;
+    @SerializedName("begin")
+    private String begin;
+    @SerializedName("end")
+    private String end;
+    @SerializedName("end_register")
+    private String end_register;
+    @SerializedName("deadline")
+    private String deadline;
+    @SerializedName("is_rdv")
+    private Boolean is_rdv;
+    @SerializedName("instance_allowed")
+    private String instance_allowed;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("description")
+    private String description;
+    @SerializedName("closed")
+    private Boolean closed;
+    @SerializedName("over")
+    private Integer over;
+    @SerializedName("over_deadline")
+    private String over_deadline;
+    @SerializedName("date_access")
+    private Boolean date_access;
+    @SerializedName("instance_registered")
+    private String instance_registered;
+    @SerializedName("user_project_status")
+    private String user_project_status;
+    @SerializedName("note")
+    private String note;
+    @SerializedName("root_slug")
+    private String root_slug;
+    @SerializedName("forum_path")
+    private String forum_path;
+    @SerializedName("slug")
+    private String slug;
+    @SerializedName("call_ihk")
+    private String call_ihk;
+    @SerializedName("nb_notes")
+    private String nb_notes;
+    @SerializedName("user_project_master")
+    private String user_project_master;
+    @SerializedName("user_project_code")
+    private String user_project_code;
+    @SerializedName("user_project_title")
+    private String user_project_title;
+    @SerializedName("registered_instance")
+    private String registered_instance;
+
+    public String getScolaryear() {
+        return scolaryear;
+    }
+
+    public void setScolaryear(String scolaryear) {
+        this.scolaryear = scolaryear;
+    }
+
+    public String getCodemodule() {
+        return codemodule;
+    }
+
+    public void setCodemodule(String codemodule) {
+        this.codemodule = codemodule;
+    }
+
+    public String getCodeinstance() {
+        return codeinstance;
+    }
+
+    public void setCodeinstance(String codeinstance) {
+        this.codeinstance = codeinstance;
+    }
+
+    public String getCodeacti() {
+        return codeacti;
+    }
+
+    public void setCodeacti(String codeacti) {
+        this.codeacti = codeacti;
+    }
+
+    public String getInstance_location() {
+        return instance_location;
+    }
+
+    public void setInstance_location(String instance_location) {
+        this.instance_location = instance_location;
+    }
+
+    public String getModule_title() {
+        return module_title;
+    }
+
+    public void setModule_title(String module_title) {
+        this.module_title = module_title;
+    }
+
+    public String getId_activitie() {
+        return id_activitie;
+    }
+
+    public void setId_activitie(String id_activitie) {
+        this.id_activitie = id_activitie;
+    }
+
+    public String getProject_title() {
+        return project_title;
+    }
+
+    public void setProject_title(String project_title) {
+        this.project_title = project_title;
+    }
+
+    public String getType_title() {
+        return type_title;
+    }
+
+    public void setType_title(String type_title) {
+        this.type_title = type_title;
+    }
+
+    public String getType_code() {
+        return type_code;
+    }
+
+    public void setType_code(String type_code) {
+        this.type_code = type_code;
+    }
+
+    public String getRegister() {
+        return register;
+    }
+
+    public void setRegister(String register) {
+        this.register = register;
+    }
+
+    public String getRegister_by_bloc() {
+        return register_by_bloc;
+    }
+
+    public void setRegister_by_bloc(String register_by_bloc) {
+        this.register_by_bloc = register_by_bloc;
+    }
+
+    public String getRegister_prof() {
+        return register_prof;
+    }
+
+    public void setRegister_prof(String register_prof) {
+        this.register_prof = register_prof;
+    }
+
+    public Integer getNb_min() {
+        return nb_min;
+    }
+
+    public void setNb_min(Integer nb_min) {
+        this.nb_min = nb_min;
+    }
+
+    public Integer getNb_max() {
+        return nb_max;
+    }
+
+    public void setNb_max(Integer nb_max) {
+        this.nb_max = nb_max;
+    }
+
+    public String getBegin() {
+        return begin;
+    }
+
+    public void setBegin(String begin) {
+        this.begin = begin;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getEnd_register() {
+        return end_register;
+    }
+
+    public void setEnd_register(String end_register) {
+        this.end_register = end_register;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    public Boolean getIs_rdv() {
+        return is_rdv;
+    }
+
+    public void setIs_rdv(Boolean is_rdv) {
+        this.is_rdv = is_rdv;
+    }
+
+    public String getInstance_allowed() {
+        return instance_allowed;
+    }
+
+    public void setInstance_allowed(String instance_allowed) {
+        this.instance_allowed = instance_allowed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getClosed() {
+        return closed;
+    }
+
+    public void setClosed(Boolean closed) {
+        this.closed = closed;
+    }
+
+    public Integer getOver() {
+        return over;
+    }
+
+    public void setOver(Integer over) {
+        this.over = over;
+    }
+
+    public String getOver_deadline() {
+        return over_deadline;
+    }
+
+    public void setOver_deadline(String over_deadline) {
+        this.over_deadline = over_deadline;
+    }
+
+    public Boolean getDate_access() {
+        return date_access;
+    }
+
+    public void setDate_access(Boolean date_access) {
+        this.date_access = date_access;
+    }
+
+    public String getInstance_registered() {
+        return instance_registered;
+    }
+
+    public void setInstance_registered(String instance_registered) {
+        this.instance_registered = instance_registered;
+    }
+
+    public String getUser_project_status() {
+        return user_project_status;
+    }
+
+    public void setUser_project_status(String user_project_status) {
+        this.user_project_status = user_project_status;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getRoot_slug() {
+        return root_slug;
+    }
+
+    public void setRoot_slug(String root_slug) {
+        this.root_slug = root_slug;
+    }
+
+    public String getForum_path() {
+        return forum_path;
+    }
+
+    public void setForum_path(String forum_path) {
+        this.forum_path = forum_path;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getCall_ihk() {
+        return call_ihk;
+    }
+
+    public void setCall_ihk(String call_ihk) {
+        this.call_ihk = call_ihk;
+    }
+
+    public String getNb_notes() {
+        return nb_notes;
+    }
+
+    public void setNb_notes(String nb_notes) {
+        this.nb_notes = nb_notes;
+    }
+
+    public String getUser_project_master() {
+        return user_project_master;
+    }
+
+    public void setUser_project_master(String user_project_master) {
+        this.user_project_master = user_project_master;
+    }
+
+    public String getUser_project_code() {
+        return user_project_code;
+    }
+
+    public void setUser_project_code(String user_project_code) {
+        this.user_project_code = user_project_code;
+    }
+
+    public String getUser_project_title() {
+        return user_project_title;
+    }
+
+    public void setUser_project_title(String user_project_title) {
+        this.user_project_title = user_project_title;
+    }
+
+    public String getRegistered_instance() {
+        return registered_instance;
+    }
+
+    public void setRegistered_instance(String registered_instance) {
+        this.registered_instance = registered_instance;
+    }
+}
