5c980b97b80bccfd489911b55bc723e1898d8e91
StuartSulaski <stuartsulaski@gmail.com>
2016-01-30 19:24:40 +0100

    Modules class updated

--- app/src/main/java/exposi_p/epidroid/models/Modules.java
+++ app/src/main/java/exposi_p/epidroid/models/Modules.java
@@ -37,122 +37,212 @@ public class Modules {
     @SerializedName("semester")
     private String semester;
 
+    /**
+     * @return The scolaryear
+     */
     public String getScolaryear() {
         return scolaryear;
     }
 
+    /**
+     * @param title The title
+     */
     public void setScolaryear(String scolaryear) {
         this.scolaryear = scolaryear;
     }
 
+    /**
+     * @return The idUserHistory
+     */
     public String getIdUserHistory() {
         return idUserHistory;
     }
 
+    /**
+     * @param title The idUserHistory
+     */
     public void setIdUserHistory(String idUserHistory) {
         this.idUserHistory = idUserHistory;
     }
 
+    /**
+     * @return The codemodule
+     */
     public String getCodemodule() {
         return codemodule;
     }
 
+    /**
+     * @param title The codemodule
+     */
     public void setCodemodule(String codemodule) {
         this.codemodule = codemodule;
     }
 
+    /**
+     * @return The codeinstance
+     */
     public String getCodeinstance() {
         return codeinstance;
     }
 
+    /**
+     * @param title The codeinstance
+     */
     public void setCodeinstance(String codeinstance) {
         this.codeinstance = codeinstance;
     }
 
+    /**
+     * @return The title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * @param title The title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * @return The idInstance
+     */
     public String getIdInstance() {
         return idInstance;
     }
 
+    /**
+     * @param title The idInstance
+     */
     public void setIdInstance(String idInstance) {
         this.idInstance = idInstance;
     }
 
+    /**
+     * @return The dateIns
+     */
     public String getDateIns() {
         return dateIns;
     }
 
+    /**
+     * @param title The dateIns
+     */
     public void setDateIns(String dateIns) {
         this.dateIns = dateIns;
     }
 
+    /**
+     * @return The cycle
+     */
     public String getCycle() {
         return cycle;
     }
 
+    /**
+     * @param title The cycle
+     */
     public void setCycle(String cycle) {
         this.cycle = cycle;
     }
 
+    /**
+     * @return The grade
+     */
     public String getGrade() {
         return grade;
     }
 
+    /**
+     * @param title The grade
+     */
     public void setGrade(String grade) {
         this.grade = grade;
     }
 
+    /**
+     * @return The credits
+     */
     public String getCredits() {
         return credits;
     }
 
+    /**
+     * @param title The credits
+     */
     public void setCredits(String credits) {
         this.credits = credits;
     }
 
+    /**
+     * @return The flags
+     */
     public String getFlags() {
         return flags;
     }
 
+    /**
+     * @param title The flags
+     */
     public void setFlags(String flags) {
         this.flags = flags;
     }
 
+    /**
+     * @return The barrage
+     */
     public String getBarrage() {
         return barrage;
     }
 
+    /**
+     * @param title The barrage
+     */
     public void setBarrage(String barrage) {
         this.barrage = barrage;
     }
 
+    /**
+     * @return The instanceId
+     */
     public String getInstanceId() {
         return instanceId;
     }
 
+    /**
+     * @param title The instanceId
+     */
     public void setInstanceId(String instanceId) {
         this.instanceId = instanceId;
     }
 
+    /**
+     * @return The moduleRating
+     */
     public String getModuleRating() {
         return moduleRating;
     }
 
+    /**
+     * @param title The moduleRating
+     */
     public void setModuleRating(String moduleRating) {
         this.moduleRating = moduleRating;
     }
 
+    /**
+     * @return The semester
+     */
     public String getSemester() {
         return semester;
     }
 
+    /**
+     * @param title The semester
+     */
     public void setSemester(String semester) {
         this.semester = semester;
     }
