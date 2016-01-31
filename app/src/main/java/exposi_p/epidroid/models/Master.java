2cee1d191ea991ba5d1cd92d86369e3818f5168c
StuartSulaski <stuartsulaski@gmail.com>
2016-01-31 19:07:26 +0100

    getter and setter added to Master class

--- app/src/main/java/exposi_p/epidroid/models/Master.java
+++ app/src/main/java/exposi_p/epidroid/models/Master.java
@@ -18,4 +18,52 @@ public class Master {
     private String picture;
     @SerializedName("title")
     private String title;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getDate_ins() {
+        return date_ins;
+    }
+
+    public void setDate_ins(String date_ins) {
+        this.date_ins = date_ins;
+    }
+
+    public String getDate_modif() {
+        return date_modif;
+    }
+
+    public void setDate_modif(String date_modif) {
+        this.date_modif = date_modif;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
