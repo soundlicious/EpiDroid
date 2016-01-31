9fc4590b8cb395fee4461e843902358df6b48db2
Emilie <emilie.dezeriaud@epitech.eu>
2016-01-31 14:26:19 +0100

    Add a lot of model for serialization. Create a new activity that take the token we get on the login activity. Request for Info with this token, request profile picture in a round ImageView \o/

--- app/src/main/java/exposi_p/epidroid/models/Projects_get.java
+++ app/src/main/java/exposi_p/epidroid/models/Projects_get.java
@@ -1,8 +1,209 @@
 package exposi_p.epidroid.models;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Emilie on 30/01/2016.
  */
 public class Projects_get {
+    @SerializedName("num_event")
+    private String numEvent;
+    @SerializedName("type_acti_code")
+    private String typeActiCode;
+    @SerializedName("project")
+    private String project;
+    @SerializedName("codeinstance")
+    private String codeinstance;
+    @SerializedName("scolaryear")
+    private String scolaryear;
+    @SerializedName("acti_title")
+    private String actiTitle;
+    @SerializedName("title_module")
+    private String titleModule;
+    @SerializedName("end_event")
+    private String endEvent;
+    @SerializedName("code_location")
+    private String codeLocation;
+    @SerializedName("begin_acti")
+    private String beginActi;
+    @SerializedName("codemodule")
+    private String codemodule;
+    @SerializedName("type_acti")
+    private String typeActi;
+    @SerializedName("codeacti")
+    private String codeacti;
+    @SerializedName("end_acti")
+    private String endActi;
+    @SerializedName("seats")
+    private String seats;
+    @SerializedName("registered")
+    private String registered;
+    @SerializedName("num")
+    private String num;
+    @SerializedName("info_creneau")
+    private String infoCreneau;
+    @SerializedName("rights")
+    private String rights;
+    @SerializedName("begin_event")
+    private String beginEvent;
 
+    public String getNumEvent() {
+        return numEvent;
+    }
+
+    public void setNumEvent(String numEvent) {
+        this.numEvent = numEvent;
+    }
+
+    public String getTypeActiCode() {
+        return typeActiCode;
+    }
+
+    public void setTypeActiCode(String typeActiCode) {
+        this.typeActiCode = typeActiCode;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public String getCodeinstance() {
+        return codeinstance;
+    }
+
+    public void setCodeinstance(String codeinstance) {
+        this.codeinstance = codeinstance;
+    }
+
+    public String getScolaryear() {
+        return scolaryear;
+    }
+
+    public void setScolaryear(String scolaryear) {
+        this.scolaryear = scolaryear;
+    }
+
+    public String getActiTitle() {
+        return actiTitle;
+    }
+
+    public void setActiTitle(String actiTitle) {
+        this.actiTitle = actiTitle;
+    }
+
+    public String getTitleModule() {
+        return titleModule;
+    }
+
+    public void setTitleModule(String titleModule) {
+        this.titleModule = titleModule;
+    }
+
+    public String getEndEvent() {
+        return endEvent;
+    }
+
+    public void setEndEvent(String endEvent) {
+        this.endEvent = endEvent;
+    }
+
+    public String getCodeLocation() {
+        return codeLocation;
+    }
+
+    public void setCodeLocation(String codeLocation) {
+        this.codeLocation = codeLocation;
+    }
+
+    public String getBeginActi() {
+        return beginActi;
+    }
+
+    public void setBeginActi(String beginActi) {
+        this.beginActi = beginActi;
+    }
+
+    public String getCodemodule() {
+        return codemodule;
+    }
+
+    public void setCodemodule(String codemodule) {
+        this.codemodule = codemodule;
+    }
+
+    public String getTypeActi() {
+        return typeActi;
+    }
+
+    public void setTypeActi(String typeActi) {
+        this.typeActi = typeActi;
+    }
+
+    public String getCodeacti() {
+        return codeacti;
+    }
+
+    public void setCodeacti(String codeacti) {
+        this.codeacti = codeacti;
+    }
+
+    public String getEndActi() {
+        return endActi;
+    }
+
+    public void setEndActi(String endActi) {
+        this.endActi = endActi;
+    }
+
+    public String getSeats() {
+        return seats;
+    }
+
+    public void setSeats(String seats) {
+        this.seats = seats;
+    }
+
+    public String getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(String registered) {
+        this.registered = registered;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getInfoCreneau() {
+        return infoCreneau;
+    }
+
+    public void setInfoCreneau(String infoCreneau) {
+        this.infoCreneau = infoCreneau;
+    }
+
+    public String getRights() {
+        return rights;
+    }
+
+    public void setRights(String rights) {
+        this.rights = rights;
+    }
+
+    public String getBeginEvent() {
+        return beginEvent;
+    }
+
+    public void setBeginEvent(String beginEvent) {
+        this.beginEvent = beginEvent;
+    }
 }
