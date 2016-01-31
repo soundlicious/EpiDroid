e7b9da1cb674b11e32b4a68d9969af2781e3bdd7
Emilie <emilie.dezeriaud@epitech.eu>
2016-01-31 21:11:44 +0100

    Add a lot of model for serialization. Create a new activity that take the token we get on the login activity. Request for Info with this token, request profile picture in a round ImageView \o/

---
+++ app/src/main/java/exposi_p/epidroid/models/Assistant.java
@@ -0,0 +1,49 @@
+package exposi_p.epidroid.models;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Emilie on 31/01/2016.
+ */
+public class Assistant {
+    @SerializedName("type")
+    private String type;
+    @SerializedName("login")
+    private String login;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("picture")
+    private String picture;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+}
